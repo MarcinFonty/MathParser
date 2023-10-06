5535a6ebc97cc494669dd131ac5dce5576d79193
Marcinkiewicz,Marcin M <m.marcinkiewicz@student.fontys.nl>
2023-10-06 11:59:55 +0200

    IDK man

---
+++ GTPAnswer/Program.cs
@@ -0,0 +1,359 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Parser
+{
+    private Scope scope;
+    private Dictionary<string, object> symbolTable = new Dictionary<string, object>();
+    private Token token;
+    private List<Token> tokens;
+    private int tokenNr;
+
+    private Token Itself()
+    {
+        return this.token;
+    }
+
+    private class Scope
+    {
+        public Dictionary<string, object> Definition { get; } = new Dictionary<string, object>();
+        public Scope Parent { get; set; }
+
+        public void Define(Token n)
+        {
+            if (Definition.TryGetValue(n.Value, out var t) && t is not null)
+            {
+                if (t is object)
+                {
+                    n.Error(t is ReservedToken ? "Already reserved." : "Already defined.");
+                }
+
+                Definition[n.Value] = n;
+                n.Reserved = false;
+                n.Nud = Itself;
+                n.Led = null;
+                n.Std = null;
+                n.Lbp = 0;
+                n.Scope = scope;
+            }
+        }
+
+        public object Find(string n)
+        {
+            var e = this;
+            object o;
+            while (true)
+            {
+                o = e.Definition.TryGetValue(n, out var val) ? val : null;
+                if (o is not null && o is not FuncToken)
+                {
+                    return o;
+                }
+
+                e = e.Parent;
+                if (e is null)
+                {
+                    o = symbolTable.TryGetValue(n, out val) ? val : symbolTable["(name)"];
+                    return o is not null && o is not FuncToken ? o : symbolTable["(name)"];
+                }
+            }
+        }
+
+        public void Pop()
+        {
+            scope = this.Parent;
+        }
+
+        public void Reserve(Token n)
+        {
+            if (n.Arity != "name" || n.Reserved)
+            {
+                return;
+            }
+
+            if (Definition.TryGetValue(n.Value, out var t) && t is not null)
+            {
+                if (t.Reserved)
+                {
+                    return;
+                }
+
+                if (t.Arity == "name")
+                {
+                    n.Error("Already defined.");
+                }
+            }
+
+            Definition[n.Value] = n;
+            n.Reserved = true;
+        }
+    }
+
+    private Scope NewScope()
+    {
+        var s = scope;
+        scope = new Scope { Parent = s };
+        return scope;
+    }
+
+    private void Advance(string id = null)
+    {
+        string a, v;
+        object o;
+        if (id != null && token.Id != id)
+        {
+            token.Error("Expected '" + id + "'.");
+        }
+
+        if (tokenNr >= tokens.Count)
+        {
+            token = symbolTable["(end)"] as Token;
+            return;
+        }
+
+        var t = tokens[tokenNr];
+        tokenNr++;
+        v = t.Value;
+        a = t.Type;
+
+        if (a == "name")
+        {
+            o = scope.Find(v);
+        }
+        else if (a == "punctuator")
+        {
+            o = symbolTable.TryGetValue(v, out var val) ? val : null;
+            if (o is null)
+            {
+                t.Error("Unknown operator.");
+            }
+        }
+        else if (a == "string" || a == "number")
+        {
+            o = symbolTable["(literal)"];
+            a = "literal";
+        }
+        else
+        {
+            t.Error("Unexpected token.");
+            o = null;
+        }
+
+        token = (Token)Activator.CreateInstance(o.GetType());
+        token.LineNr = t.LineNr;
+        token.ColumnNr = t.ColumnNr;
+        token.Value = v;
+        token.Arity = a;
+    }
+
+    private Token Expression(int rbp)
+    {
+        Token left;
+        var t = token;
+        Advance();
+        left = t.Nud();
+        while (rbp < token.Lbp)
+        {
+            t = token;
+            Advance();
+            left = t.Led(left);
+        }
+        return left;
+    }
+
+    private Token Statement()
+    {
+        var n = token;
+        Token v;
+
+        if (n.Std != null)
+        {
+            Advance();
+            scope.Reserve(n);
+            return n.Std();
+        }
+        v = Expression(0);
+        if (!v.Assignment && v.Id != "(")
+        {
+            v.Error("Bad expression statement.");
+        }
+        Advance(";");
+        return v;
+    }
+
+    private List<Token> Statements()
+    {
+        var a = new List<Token>();
+        Token s;
+        while (true)
+        {
+            if (token.Id == "}" || token.Id == "(end)")
+            {
+                break;
+            }
+            s = Statement();
+            if (s != null)
+            {
+                a.Add(s);
+            }
+        }
+        return a.Count == 0 ? null : a.Count == 1 ? new List<Token> { a[0] } : a;
+    }
+
+    private Token Block()
+    {
+        var t = token;
+        Advance("{");
+        return t.Std();
+    }
+
+    private class OriginalSymbol : Token
+    {
+        public OriginalSymbol()
+        {
+            Nud = () => Error("Undefined.");
+            Led = left => Error("Missing operator.");
+        }
+    }
+
+    private Token Symbol(string id, int bp = 0)
+    {
+        var s = symbolTable.TryGetValue(id, out var val) ? val : null;
+        bp = bp > 0 ? bp : 0;
+        if (s != null)
+        {
+            if (bp >= s.Lbp)
+            {
+                s.Lbp = bp;
+            }
+        }
+        else
+        {
+            s = new OriginalSymbol { Id = id, Value = id, Lbp = bp };
+            symbolTable[id] = s;
+        }
+        return s;
+    }
+
+    private Token Constant(string s, object v)
+    {
+        var x = Symbol(s);
+        x.Nud = () =>
+        {
+            scope.Reserve(this);
+            this.Value = symbolTable[this.Id].Value;
+            this.Arity = "literal";
+            return this;
+        };
+        x.Value = v;
+        return x;
+    }
+
+    private Token Infix(string id, int bp, Func<Token, Token> led = null)
+    {
+        var s = Symbol(id, bp);
+        s.Led = led ?? (left =>
+        {
+            this.First = left;
+            this.Second = Expression(bp);
+            this.Arity = "binary";
+            return this;
+        });
+        return s;
+    }
+
+    private Token InfixR(string id, int bp, Func<Token, Token> led = null)
+    {
+        var s = Symbol(id, bp);
+        s.Led = led ?? (left =>
+        {
+            this.First = left;
+            this.Second = Expression(bp - 1);
+            this.Arity = "binary";
+            return this;
+        });
+        return s;
+    }
+
+    private Token Assignment(string id)
+    {
+        return InfixR(id, 10, left =>
+        {
+            if (left.Id != "." && left.Id != "[" && left.Arity != "name")
+            {
+                left.Error("Bad lvalue.");
+            }
+            this.First = left;
+            this.Second = Expression(9);
+            this.Assignment = true;
+            this.Arity = "binary";
+            return this;
+        });
+    }
+
+    private Token Prefix(string id, Func<Token> nud = null)
+    {
+        var s = Symbol(id);
+        s.Nud = nud ?? (() =>
+        {
+            scope.Reserve(this);
+            this.First = Expression(70);
+            this.Arity = "unary";
+            return this;
+        });
+        return s;
+    }
+
+    private Token Stmt(string s, Func<Token> f)
+    {
+        var x = Symbol(s);
+        x.Std = f;
+        return x;
+    }
+
+    public Token Parse(List<Token> arrayOfTokens)
+    {
+        tokens = arrayOfTokens;
+        tokenNr = 0;
+        NewScope();
+        Advance();
+        var s = Statements();
+        Advance("(end)");
+        scope.Pop();
+        return s;
+    }
+}
+
+public class Token
+{
+    public string Type { get; set; }
+    public string Value { get; set; }
+    public int LineNr { get; set; }
+    public int ColumnNr { get; set; }
+    public string Id { get; set; }
+    public string Arity { get; set; }
+    public bool Reserved { get; set; }
+    public bool Assignment { get; set; }
+
+    public void Error(string message)
+    {
+        throw new Exception($"Error at Line {LineNr}, Column {ColumnNr}: {message}");
+    }
+
+    public virtual Token Nud()
+    {
+        Error("Undefined.");
+        return this;
+    }
+
+    public virtual Token Led(Token left)
+    {
+        Error("Missing operator.");
+        return this;
+    }
+}
---
+++ MathParserV4/Program.cs
@@ -0,0 +1,259 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //string input = Console.ReadLine();
+        Token[] tokens = Tokenize(Console.ReadLine());
+        Parser parser = new Parser(tokens);
+
+        foreach (var token in tokens) //uses this to ckeck if all the tokens are set correctly.
+        {
+            Console.WriteLine($"Type: {token.type}, Value: {token.value}");
+        }
+
+        Console.WriteLine("parsing succeeded");
+    }
+
+    static Token[] Tokenize(string input) //TODO: update the tokenizer to also set the Id for the tocken
+    {
+        Token[] tokens = new Token[input.Length]; // Create an array with the same length as the input string
+        int tokenIndex = 0;
+
+        string currentToken = "";
+
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
+                currentToken += c;
+            }
+            else if (IsOperator(c))
+            {
+                // Add the current number token (if any)
+                if (!string.IsNullOrEmpty(currentToken))
+                {
+                    tokens[tokenIndex++] = new Token { type = Type.literal, value = int.Parse(currentToken) };
+                    currentToken = "";
+                }
+
+                tokens[tokenIndex++] = new Token { type = Type.literal, value = c.ToString() };
+            }
+            else if (!char.IsWhiteSpace(c))
+            {
+                throw new Exception($"Invalid character: {c}");
+            }
+        }
+
+        // Add the last number token (if any)
+        if (!string.IsNullOrEmpty(currentToken))
+        {
+            tokens[tokenIndex++] = new Token { type = Type.literal, value = int.Parse(currentToken) };
+        }
+
+        // Resize the array to the actual number of tokens
+        Array.Resize(ref tokens, tokenIndex);
+
+        return tokens;
+    }
+
+    static bool IsOperator(char c) //TODO: probably could change the brackets to it's own type instead of operator or literal/number
+    {
+        return "+-*/()".Contains(c);
+    }
+}
+
+internal class Parser
+{
+    public Dictionary<string, Symbol> symbolTable = new Dictionary<string, Symbol>();
+    public Token[] tokens; 
+    public Token currentToken; 
+    public int token_nr = 0; 
+    public Parser(Token[] input)
+    {
+        tokens = input;
+        currentToken = input[0];
+
+        AddOrUpdateSymbol(")");
+
+        Infix("+", 50);
+        Infix("-", 50);
+        Infix("*", 60);
+        Infix("/", 60);
+        InfixR("^", 70);
+        //InfixR("√", 70);
+
+        AddOrUpdateSymbol("(literal)").Nud = () =>
+        {
+            return AddOrUpdateSymbol("(literal)");
+        };
+    }
+
+    public Symbol AddOrUpdateSymbol(string id, int bp = 0)
+    {
+        if (symbolTable.ContainsKey(id))
+        {
+            if (bp >= symbolTable[id].LeftBindingPower)
+            {
+                symbolTable[id].LeftBindingPower = bp;
+            }
+        }
+        else 
+        {
+            symbolTable[id] = new Symbol(bp);
+        }
+
+        return symbolTable[id];
+    }
+
+    public Symbol Infix(string id, int bp, Func<Symbol, Symbol> led = null)
+    {
+        var s = AddOrUpdateSymbol(id, bp);
+        if (led == null)
+        {
+            s.Led = (left) =>
+            {
+                s.First = left;
+                s.Second = Expression(bp);
+                s.Arity = "binary";
+                return s;
+            };
+        }
+        else
+        {
+            s.Led = led;
+        }
+
+        return s;
+    }
+
+    public Symbol InfixR(string id, int bp, Func<Symbol, Symbol> led = null)
+    {
+        var s = AddOrUpdateSymbol(id, bp);
+        if (led == null)
+        {
+            s.Led = (left) =>
+            {
+                s.First = left;
+                s.Second = Expression(bp - 1);
+                s.Arity = "binary";
+                return s;
+            };
+        }
+        else
+        {
+            s.Led = led;
+        }
+
+        return s;
+    }
+
+    public Symbol Expression(int RightBindingPower)
+    {
+        var left = new Symbol();
+        var t = currentToken;
+        Advance();
+        left = t.Nud();
+        while (RightBindingPower < currentToken.LeftBindingPower)
+        {
+            t = currentToken;
+            Advance();
+            left = t.Led(left);
+        }
+        return left;
+    }
+
+    public Token Advance(string id = null) //transforms a simple token of the inputed tokens into one I can work with, aka takes from tokens and token_nr to make the currentToken. Important to understand tokens[token_nr] != currentToken;
+    {
+        Type a;
+        Symbol o;
+        object v;
+
+        if (id != null && currentToken.Id != id) //TODO: I don't understand this one, why does token contain definition of id as it already posseses it's value? Unsure about this but just copied the example code.
+        {
+            throw new Exception("Expected '" + id + "'.");
+        }
+        if (token_nr >= tokens.Length)
+        {
+            currentToken = new Token(); //unsure if I should use a new token or update the last token for the end. I'm guessing a new one is more suitable.
+            currentToken.AddSymbolValues(symbolTable["(end)"]);
+        }
+
+        var t = tokens[token_nr];
+        token_nr++;
+        v = t.value;
+        a = t.type;
+
+        if (a == Type.Operator)
+        {
+            if (!symbolTable.TryGetValue(v.ToString(), out o)) //sets the value of o, if key for o doesn't exist it goes inside the if statment and throws an exeption. This allows for a more gracefull error handling (which I don't do as I just throw it)
+            {
+                throw new Exception("unknow operator");
+            }
+        }
+        else if (a == Type.literal)
+        {
+            o = symbolTable["(literal)"];
+        }
+        else
+        {
+            throw new Exception("unexpected token");
+        }
+
+        currentToken = new Token();
+        currentToken.AddSymbolValues(o);
+        currentToken.value = v;
+        currentToken.type = a;
+
+        return currentToken;
+    }
+}
+
+internal class Token : Symbol
+{
+    public string Id { get; set; }
+    public Type type { get; set; }
+    public object value { get; set; }
+
+    public void AddSymbolValues(Symbol symbol)
+    {
+        this.LeftBindingPower = symbol.LeftBindingPower;
+        this.Led = symbol.Led;
+        this.Nud = symbol.Nud;
+    }
+}
+
+internal class Symbol : BaseSymbol
+{
+    public int LeftBindingPower { get; set; }
+
+    // Properties to store left and right operands
+    public Symbol First { get; set; }
+    public Symbol Second { get; set; }
+
+    // Property to specify the arity of the symbol (e.g., "binary")
+    public string Arity { get; set; }
+    public Symbol(int bp = 0)
+    {
+        LeftBindingPower = bp;
+    }
+}
+
+internal class BaseSymbol //TODO: Posibly consider making these function abstracts
+{
+    public Func<Symbol> Nud { get; set; } = () =>
+    {
+        throw new Exception("Undefiened");
+    };
+
+    public Func<Symbol, Symbol> Led { get; set; } = (left) =>
+    {
+        throw new Exception("Missing operator");
+    };
+}
+
+internal enum Type
+{
+    Operator,
+    literal,
+    variable
+}
